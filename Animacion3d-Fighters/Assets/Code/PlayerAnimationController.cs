5152f367dac104ea21080c3785ae7f84a2432ee7
Galycon <2004rasr@gmail.com>
2025-01-11 12:53:20 +0100

    RSR - Input mando

--- Animacion3d-Fighters/Assets/Code/PlayerAnimationController.cs
+++ Animacion3d-Fighters/Assets/Code/PlayerAnimationController.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     private Animator animator;
+    public int health = 100; // Vida del jugador
+    public GameObject opponent; // Referencia al oponente
+    private bool isAttacking = false; // Para verificar si el ataque está activo
+
+    public BoxCollider slowAttackHitbox;
+    public BoxCollider fastAttackHitbox;
+    public BoxCollider lowAttackHitbox;
 
     void Start()
     {
@@ -16,59 +23,111 @@ public class PlayerAnimationController : MonoBehaviour
 
     private void HandleInput()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.J))
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Stick izquierdo para moverse
+        bool blockButton = Input.GetButton("Fire2");        // Botón Fire2 para bloquear
+        bool quickAttackButton = Input.GetButtonDown("Fire3"); // Botón Fire1 para ataque rápido
+        bool slowAttackButton = Input.GetButtonDown("Fire1");  // Botón Fire3 para ataque lento
+        bool downInput = Input.GetAxis("Vertical") < -0.5f; // Stick abajo
+        bool upInput = Input.GetAxis("Vertical") > 0.5f;    // Stick arriba
+
+        // Movimiento
+        if (moveHorizontal > 0)
         {
-            PlayAnimation("SlowLowAttack");
+            PlayAnimation("MoveRight");
+        }
+        else if (moveHorizontal < 0)
+        {
+            PlayAnimation("MoveLeft");
+        }
+        else
+        {
+            PlayAnimation("Idle");
         }
 
-        else if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.Space))
+        // Esquivar y Evadir
+        if (downInput && blockButton)
         {
             PlayAnimation("DodgeHighAttack");
         }
-
-        else if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.J))
-        {
-            PlayAnimation("LowAttack");
-        }
-
-        else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.J))
-        {
-            PlayAnimation("SlowAttack");
-        }
-
-        else if (Input.GetKeyDown(KeyCode.J))
-        {
-            PlayAnimation("FastAttack");
-        }
-
-        else if (Input.GetKeyDown(KeyCode.Space))
+        else if (upInput && blockButton)
         {
             PlayAnimation("DodgeAttack");
         }
 
-        else if (Input.GetKeyDown(KeyCode.N))
+        // Ataques
+        else if (downInput && quickAttackButton)
         {
-            PlayAnimation("Win");
+            PlayAnimation("LowAttack");
+            TriggerAttack("LowAttack", lowAttackHitbox);
         }
-
-        else if (Input.GetKeyDown(KeyCode.M))
+        else if (downInput && slowAttackButton)
         {
-            PlayAnimation("Lose");
+            PlayAnimation("SlowLowAttack");
+            TriggerAttack("SlowLowAttack", lowAttackHitbox);
         }
-
-        else if (Input.GetKey(KeyCode.A))
+        else if (quickAttackButton)
         {
-            PlayAnimation("MoveLeft");
+            PlayAnimation("FastAttack");
+            TriggerAttack("FastAttack", fastAttackHitbox);
         }
-
-        else if (Input.GetKey(KeyCode.D))
+        else if (slowAttackButton)
         {
-            PlayAnimation("MoveRight");
+            PlayAnimation("SlowAttack");
+            TriggerAttack("SlowAttack", slowAttackHitbox);
         }
     }
 
     private void PlayAnimation(string animationTrigger)
     {
         animator.SetTrigger(animationTrigger);
+    }
+
+    private void TriggerAttack(string attackTrigger, BoxCollider attackHitbox)
+    {
+        if (attackHitbox == null) return;
+
+        isAttacking = true;
+
+        // Activar la hitbox correspondiente
+        attackHitbox.enabled = true;
+
+        // Desactivar la hitbox al finalizar el ataque
+        Invoke(nameof(ResetAttack), 0.5f); // Ajusta el tiempo según la duración de la animación
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Comprueba si se golpea al oponente
+        if (isAttacking && other.gameObject == opponent)
+        {
+            // Quita toda la vida al oponente (solo para pruebas)
+            PlayerAnimationController opponentController = opponent.GetComponent<PlayerAnimationController>();
+            if (opponentController != null)
+            {
+                opponentController.TakeDamage(100); // Daño total para testear
+            }
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log($"{gameObject.name} recibió {damage} de daño. Vida restante: {health}");
+
+        if (health <= 0)
+        {
+            PlayAnimation("Lose");
+            Debug.Log($"{gameObject.name} ha sido derrotado.");
+        }
+    }
+
+    private void ResetAttack()
+    {
+        isAttacking = false;
+
+        // Desactivar todas las hitboxes
+        if (slowAttackHitbox != null) slowAttackHitbox.enabled = false;
+        if (fastAttackHitbox != null) fastAttackHitbox.enabled = false;
+        if (lowAttackHitbox != null) lowAttackHitbox.enabled = false;
     }
 }
